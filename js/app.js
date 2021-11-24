e82be2b81411e62032e4a885a582833000dbbdb7
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-24 14:08:29 +0100

    revision

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const LEFT_BUTTON = 0;
+const RIGHT_BUTTON = 2;
+const cases = document.getElementsByClassName("case");
+
+//suppr menu
+document.addEventListener('contextmenu', function (event){
+    event.preventDefault();
+});
+
+for (let i =0; i < cases.length; i++) {
+    cases[i].addEventListener("mouseup", function (event){
+        if (event.button === LEFT_BUTTON)
+                inserText(this, "X", "green");
+
+        else if (event.button === RIGHT_BUTTON)
+                inserText(this, "O", "red");
+
+    });
+}
+
+/**
+ *
+ * @param element
+ * @param lettreJoueur
+ * @param classCss
+ */
+function inserText (element, lettreJoueur, classCss) {
+    element.innerText = lettreJoueur;
+    element.classList.add(classCss);
+    element.style.backgroundColor = "#DEDeDe";
+}
